70fda9562157f1c8d424e3a845aa455389db3af9
nitin1518 <riding.driving@gmail.com>
2015-02-18 19:14:44 +0530

    Initial Commit

---
+++ dateBarRate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dateBarRate
+//
+//  Created by Nitin Sharma on 18/02/15.
+//  Copyright (c) 2015 Nitin Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
